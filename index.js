d12d9813dbce067f8e85e70c10bb508b944c0102
camilo otalora <cesarcamiloo895@gmail.com>
2022-12-19 23:50:36 -0500

    JS estructurado 1

---
+++ index.js
@@ -0,0 +1,123 @@
+const frutas = ["manzana", "pera", "banano", "coco", "uva", "fresa", "mora", "sandia", "maracuya", "piña", "mango", "cereza","kiwi","melon","durazno"]
+console.table(frutas)
+const descuento = frutas.filter
+
+const manzana = {
+    id:0,
+    producto: "manzana",
+    precio: "$5",
+    descripcion: "precio por libra / manzana roja cosecha alta",
+    oferta: true,
+}
+
+const pera = {
+    id:1,
+    producto: "pera",
+    precio: "$7",
+    descripcion: "precio por libra / pera cosecha baja",
+    oferta: false,
+}
+
+const banano = {
+    id:2,
+    producto: "banano",
+    precio: "$5",
+    descripcion: "precio por libra / banano cosecha alta",
+    oferta: true,
+}
+
+const coco = {
+    id:3,
+    producto: "coco",
+    precio: "$10",
+    descripcion: "precio por libra / coco cosecha baja",
+    oferta: false,
+}
+
+const uva = {
+    id:4,
+    producto: "uva",
+    precio: "$4",
+    descripcion: "precio por libra / uva cosecha alta",
+    oferta: true,
+}
+
+const fresa = {
+    id:5,
+    producto: "fresa",
+    precio: "$7",
+    descripcion: "precio por libra / fresa cosecha baja",
+    oferta: false,
+}
+
+const mora = {
+    id:6,
+    producto: "mora",
+    precio: "$6",
+    descripcion: "precio por libra / mora cosecha baja",
+    oferta: false,
+}
+
+const sandia = {
+    id:7,
+    producto: "sandia",
+    precio: "$12",
+    descripcion: "precio por libra / sandia cosecha baja ",
+    oferta: true,
+}
+
+const maracuya = {
+    id:8,
+    producto: "maracuya",
+    precio: "$6",
+    descripcion: "precio por libra / maracuya cosecha alta",
+    oferta: false,
+}
+
+const piña = {
+    id:9,
+    producto: "piña",
+    precio: "$12",
+    descripcion: "precio por libra / piña cosecha baja",
+    oferta: false,
+}
+
+const mango = {
+    id:10,
+    producto: "mango",
+    precio: "$8",
+    descripcion: "precio por libra / mango cosecha alta",
+    oferta: true,
+}
+
+const cereza = {
+    id:11,
+    producto: "cereza",
+    precio: "$7",
+    descripcion: "precio por libra / cereza cosecha baja",
+    oferta: false,
+}
+
+const kiwi = {
+    id:12,
+    producto: "kiwi",
+    precio: "$9",
+    descripcion: "precio por libra / kiwi cosecha baja",
+    oferta: false,
+}
+
+const melon = {
+    id:13,
+    producto: "melon",
+    precio: "$11",
+    descripcion: "precio por libra / melon cosecha baja",
+    oferta: false,
+}
+
+const durazno = {
+    id:14,
+    producto: "durazno",
+    precio: "$8",
+    descripcion: "precio por libra / durazno cosecha baja",
+    oferta: false,  
+}
